1a5d8c85faeee66ed7c6b6ac37266ae5f8e95426
Shreyans Jain <50544190+Shreyans13@users.noreply.github.com>
2019-09-28 02:00:27 +0530

    Create Day 6: JavaScript Dates.js

---
+++ Day 6: JavaScript Dates.js
@@ -0,0 +1,42 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+function getDayName(dateString) {
+    let date = new Date(dateString);
+    let dayName=date.getDay();
+    var a=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    // Write your code here
+    
+    return a[dayName];
+}
+
+
+function main() {
+    const d = +(readLine());
+    
+    for (let i = 0; i < d; i++) {
+        const date = readLine();
+        
+        console.log(getDayName(date));
+    }
+}
