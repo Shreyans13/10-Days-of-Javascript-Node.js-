3b9cffb95c29f2ea6b4c6af49816d61cfec85a72
Shreyans Jain <50544190+Shreyans13@users.noreply.github.com>
2019-09-28 04:02:36 +0530

    Create Day 7: Regular Expressions II.js

---
+++ Day 7: Regular Expressions II.js
@@ -0,0 +1,33 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+function regexVar() {
+return '^(Mr|Mrs|Ms|Dr|Er)(\\.)([A-Za-z])*$';
+}
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    
+    console.log(!!s.match(re));
+}
